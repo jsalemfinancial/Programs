7b47b64136b19830fb5beacfcaac7559123b7932
jsalemfinancial <87406127+jsalemfinancial@users.noreply.github.com>
2023-03-25 20:56:31 -0400

    Moved Project to New Repo

--- Flask Project/flaskProj/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, session
-from flask_bcrypt import Bcrypt
-
-app = Flask(__name__.split(".")[0])
-app.config.from_object("config.Config")
-
-flaskBcrypt = Bcrypt(app)
-
-from flaskProj import routes
